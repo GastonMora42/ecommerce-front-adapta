3b326cdb6b29e3b44b0f269f3d21c599fcdb66ba
GastonMora42 <gastonmora1742@gmail.com>
2024-06-25 14:14:22 -0300

    Edge functions

--- edge-functions/hello.js
+++ edge-functions/hello.js
@@ -1,11 +1,26 @@
+import { NextResponse } from 'next/server';
+ 
+// The country to block from accessing the secret page
+const BLOCKED_COUNTRY = 'SE';
+ 
+// Trigger this middleware to run on the `/secret-page` route
 export const config = {
-    runtime: 'edge',
-  };
-  
-  export default async function handler(req) {
-    const { searchParams } = new URL(req.url);
-    const name = searchParams.get('name') || 'World';
-  
-    return new Response(`Hello, ${name}!`);
+  matcher: '/secret-page',
+};
+ 
+export default function middleware(request) {
+  // Extract country. Default to US if not found.
+  const country = (request.geo && request.geo.country) || 'US';
+ 
+  console.log(`Visitor from ${country}`);
+ 
+  // Specify the correct route based on the requests location
+  if (country === BLOCKED_COUNTRY) {
+    request.url.pathname = '/create-order.js';
+  } else {
+    request.url.pathname = `/create-preference.js`;
   }
-  
+ 
+  // Rewrite to URL
+  return NextResponse.rewrite(request.url);
+}
