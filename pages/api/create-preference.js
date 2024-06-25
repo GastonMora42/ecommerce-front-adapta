3cf6dac8fa2776f747d2f1b17d81589d0c7cb125
GastonMora42 <gastonmora1742@gmail.com>
2024-06-24 23:05:49 -0300

    Fix

--- pages/api/create-preference.js
+++ pages/api/create-preference.js
@@ -1,54 +1,30 @@
 import { MercadoPagoConfig, Preference } from 'mercadopago';
-import { mongooseConnect } from "@/lib/mongoose";
-import { Order } from "@/models/Order";
 
-// Configurar MercadoPago usando una variable de entorno para la clave de accesos
 const client = new MercadoPagoConfig({
-  accessToken: process.env.MERCADO_PAGO_ACCESS_TOKEN
+  accessToken: process.env.MERCADO_PAGO_ACCESS_TOKEN, // Usar variables de entorno
 });
 
 const preference = new Preference(client);
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    // Conectarse a la base de datos una vez al inicio
-    if (!global.mongooseConnected) {
-      await mongooseConnect();
-      global.mongooseConnected = true;
-    }
-
     try {
-      const { items, name, email, city, postalCode, streetAddress, country } = req.body;
+      const { items } = req.body;
 
-      // Crear la orden en la base de datos
-      const order = new Order({
-        line_items: items,
-        name,
-        email,
-        city,
-        postalCode,
-        streetAddress,
-        country,
-        paid: false, // Inicialmente no está pagada
-      });
-      await order.save();
-
-      // Crear la preferencia de MercadoPago
       const preferenceBody = {
         items,
         back_urls: {
           success: 'https://www.youtube.com/@onthecode',
           failure: 'https://www.youtube.com/@onthecode',
-          pending: 'https://www.youtube.com/@onthecode'
+          pending: 'https://www.youtube.com/@onthecode',
         },
         auto_return: 'approved',
-        external_reference: order._id.toString(), // Pasar la referencia de la orden
-        notification_url: 'https://adaptalabs/api/webhook', // URL del webhook
       };
 
       const result = await preference.create({ body: preferenceBody });
 
-      // Verificar y responder con el ID de la preferencia creada
+      console.log('MercadoPago create response:', result);
+
       if (result && result.id) {
         res.status(200).json({ id: result.id });
       } else {
