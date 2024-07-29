a3afcae8b5496816a3fcb9d2dfe32f97601b9a3a
GastonMora42 <gastonmora1742@gmail.com>
2024-07-29 14:23:10 -0300

    Commit

--- pages/api/webhoock.js
+++ pages/api/webhoock.js
@@ -57,12 +57,14 @@ export default async function handler(req, res) {
         console.error('Invalid type in webhook notification.');
       }
 
+      // Responder con 200 OK
       res.status(200).send('OK');
     } catch (error) {
       console.error('Error processing webhook:', error);
       res.status(500).json({ error: 'Error processing webhook' });
     }
   } else {
+    // Responder con Method Not Allowed si no es POST
     res.setHeader('Allow', ['POST']);
     res.status(405).json({ error: 'Method Not Allowed' });
   }
